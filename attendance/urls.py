c7c4a80684d3c7b19dc8f22447e0b83a809d60b5
Noor Ahmed <noor@chobirbaksho.com>
2021-09-29 19:43:40 +0600

    url fix

--- attendance/urls.py
+++ attendance/urls.py
@@ -4,5 +4,5 @@ from .views import Payload, TimesheetHandler, SlackUserCreation
 
 urlpatterns = [
     path('slack/integration/', SlackUserCreation.as_view(), name="slack-integration"),
-    path("attendance/slack/", TimesheetHandler.as_view(), name="timesheet"),
+    path("slack/", TimesheetHandler.as_view(), name="timesheet"),
 ]
