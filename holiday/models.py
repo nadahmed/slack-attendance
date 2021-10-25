b91f5df64b873d0c5fcfbadb147625e9a864738d
Noor Ahmed <noor@chobirbaksho.com>
2021-10-25 15:29:23 +0600

    holiday view bulk update added & subject_to_change model field added

--- holiday/models.py
+++ holiday/models.py
@@ -4,7 +4,11 @@ class Holiday(models.Model):
     name = models.CharField(max_length=100)
     date = models.DateField()
     is_active = models.BooleanField(default=True)
+    subject_to_change = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+    
     class Meta:
         ordering = ['date']
 
--- holiday/views.py
+++ holiday/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
 from holiday.models import Holiday
 from holiday.serializers import HolidaySerializer
 import django_filters
@@ -22,3 +24,17 @@ class HolidayViewSet(viewsets.ModelViewSet):
     queryset = Holiday.objects.all()
     filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
     filter_class = HolidayFilter
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.get('items', request.data)
+        many = isinstance(data, list)
+        print (data, many)
+        serializer = self.get_serializer(data=data, many=many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED,
+                headers=headers
+        )
